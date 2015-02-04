b639300e6f7cd0e3b6930a80bc85f7d4c4f4462a
Alister A. Fenix <a.fenix@icloud.com>
2015-02-03 17:06:25 -0800

    Add a form to gather start and end locations for the map feature. Also added a rudimentary leaflet map.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'multi_json'
 require 'bundler/setup'
+require 'pry'
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
@@ -13,4 +14,22 @@ end
 
 get '/' do
   erb :home
+end
+
+get '/form' do
+  erb :form
+end
+
+post '/form' do
+  @starting = params.fetch("starting")
+  @ending = params.fetch("ending")
+  erb :success
+end
+
+get '/success' do
+ erb :success
+end
+
+get '/map' do
+  erb :map
 end
